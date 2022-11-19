d717af64c2edb1913b967044c83e9c66033be6e4
Gordanna7 <gordannaa4@gmail.com>
2022-11-19 17:35:25 +0100

    Objects and Arrays

---
+++ Homework2/JS/script.js
@@ -0,0 +1,28 @@
+//OBJECTS
+
+var favrecipe = {
+    title: "Mole",
+    servings: 2,
+    ingredients: [" \ncinnamon,\n cumin,\n cocoa"]
+};
+
+console.log(favrecipe.title);
+console.log("Servings:" + favrecipe.servings);
+console.log("Ingredients:" + favrecipe.ingredients);
+
+
+//ARRAYS
+
+var programmingLanguages = [
+    "C++", "C#", "Ruby", "Java", "JavaScript", "Python", "PHP", ".net"
+];
+
+console.log(programmingLanguages.length);
+console.log(programmingLanguages);
+
+console.log(programmingLanguages.pop());
+console.log(programmingLanguages); 
+
+
+var removeAdd = programmingLanguages.splice(2, 1, "SQL");
+console.log(programmingLanguages);
